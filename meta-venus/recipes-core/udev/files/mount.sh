de9f95a3e7eb4273f671019109504c5e3a9fc85c
Jeroen Hofstee <jhofstee@victronenergy.com>
2016-10-19 17:11:31 +0200

    mount.sh: make it work on danny as well
    - remove ID_FS_TYPE reference, since it is not set.

--- meta-venus/recipes-core/udev/files/mount.sh
+++ meta-venus/recipes-core/udev/files/mount.sh
@@ -17,7 +17,7 @@ do
 	fi
 done
 
-automount() {	
+automount() {
 	name="`basename "$DEVNAME"`"
 
 	! test -d "/media/$name" && mkdir -p "/media/$name"
@@ -26,17 +26,6 @@ automount() {
 	then
 		MOUNT="$MOUNT -o silent"
 	fi
-	
-	# If filesystem type is vfat, change the ownership group to 'disk', and
-	# grant it with  w/r/x permissions.
-	case $ID_FS_TYPE in
-	vfat|fat)
-		MOUNT="$MOUNT -o umask=007,gid=`awk -F':' '/^disk/{print $3}' /etc/group`"
-		;;
-	# TODO
-	*)
-		;;
-	esac
 
 	if ! $MOUNT -t auto $DEVNAME "/media/$name"
 	then
@@ -48,7 +37,7 @@ automount() {
 		dbus-send --system --type=signal / com.victronenergy.udev.mount string:$DEVNAME string:"/media/$name"
 	fi
 }
-	
+
 rm_dir() {
 	# We do not want to rm -r populated directories
 	if test "`find "$1" | wc -l | tr -d " "`" -lt 2 -a -d "$1"
@@ -63,13 +52,13 @@ rm_dir() {
 name="`basename "$DEVNAME"`"
 [ -e /sys/block/$name/device/media ] && media_type=`cat /sys/block/$name/device/media`
 
-if [ "$ACTION" = "add" ] && [ -n "$DEVNAME" ] && [ -n "$ID_FS_TYPE" -o "$media_type" = "cdrom" ]; then
+if [ "$ACTION" = "add" ] && [ -n "$DEVNAME" ]; then
 	if [ -x "$PMOUNT" ]; then
 		$PMOUNT $DEVNAME 2> /dev/null
 	elif [ -x $MOUNT ]; then
     		$MOUNT $DEVNAME 2> /dev/null
 	fi
-	
+
 	# If the device isn't mounted at this point, it isn't
 	# configured in fstab (note the root filesystem can show up as
 	# /dev/root in /proc/mounts, so check the device number too)
