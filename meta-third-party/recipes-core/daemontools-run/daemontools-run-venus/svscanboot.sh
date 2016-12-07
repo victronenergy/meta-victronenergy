1e25016b071cd12a15f2ecfcda6cb91c1b00bcc4
Jeroen Hofstee <jhofstee@victronenergy.com>
2016-12-07 10:57:01 +0100

    svscanboot: log startup and reason to all log files

--- meta-third-party/recipes-core/daemontools-run/daemontools-run-venus/svscanboot.sh
+++ meta-third-party/recipes-core/daemontools-run/daemontools-run-venus/svscanboot.sh
@@ -2,6 +2,12 @@
 
 case "$1" in
 'start')
+	# Log to indicate (re)start
+	boot=`cat /tmp/last_boot_type`
+	if [ "$boot" != "-3" ]; then
+		for log in `ls /log/*/current 2> /dev/null`; do echo "*** CCGX booted ($boot) ***" | tai64n >> $log; done
+	fi
+	sync
 	svscanboot &
 	;;
 'stop')
