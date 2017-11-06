e9071419e9ba0602a1140a5ce84c005947a46d68
Jeroen Hofstee <jhofstee@victronenergy.com>
2017-11-06 13:05:16 +0100

    meta-toolchain-qte: make the environmental script optional
    qmake inside the SDK relies on the enviromental script for CC and the
    sysroot location. This patches the mkspecs after installation, so sourcing
    the script becomes optional.

---
+++ meta-venus/recipes-qt/meta/files/hardcode-qt-mkspec-paths.sh
@@ -0,0 +1,46 @@
+set -e
+
+# The default meta-toolchain-qte relies on the environmental script being sourced.
+# Although that works, it doesn't work well in combination with Qt Creator, since
+# an user can no longer switch between Qt version, or has to manually set a whole
+# bunch of build environments.
+#
+# In order to prevent that all paths / commands are hardcoded in the mkspecs after
+# the SDK has been installed. In order to do that the SDK calls this script as a
+# post install command with the envirmental script name as argument. This script
+# is sourced and required OE variables in the mkspecs are replaced with their values
+# so sourcing the script is no longer needed for Qt builds.
+
+environment="$1"
+. "${environment}"
+
+printf "Updating qt4 mkspecs\n"
+sed -i \
+	-e "s|\$(OE_QMAKE_CFLAGS)|${CFLAGS}|g" \
+	-e "s|\$(OE_QMAKE_CXXFLAGS)|${CXXFLAGS}|g" \
+	-e "s|\$(OE_QMAKE_LDFLAGS)|${LDFLAGS}|g" \
+	-e "s|\$(OE_QMAKE_CC)|${CC}|g" \
+	-e "s|\$(OE_QMAKE_CXX)|${CXX}|g" \
+	-e "s|\$(OE_QMAKE_LINK)|${CXX}|g" \
+	-e "s|\$(OE_QMAKE_AR)|${AR}|g" \
+	-e "s|\$(OE_QMAKE_LIBDIR_QT)|$OECORE_TARGET_SYSROOT/usr/lib|g" \
+	-e "s|\$(OE_QMAKE_INCDIR_QT)|$OECORE_TARGET_SYSROOT/usr/include/qtopia|g" \
+	-e "s|\$(OE_QMAKE_MOC)|$OECORE_NATIVE_SYSROOT/usr/bin/moc4|g" \
+	-e "s|\$(OE_QMAKE_UIC)|$OECORE_NATIVE_SYSROOT/usr/bin/uic4|g" \
+	-e "s|\$(OE_QMAKE_UIC3)|$OECORE_NATIVE_SYSROOT/usr/bin/uic34|g" \
+	-e "s|\$(OE_QMAKE_RCC)|$OECORE_NATIVE_SYSROOT/usr/bin/rcc4|g" \
+	-e "s|\$(OE_QMAKE_QDBUSCPP2XML)|$OECORE_NATIVE_SYSROOT/usr/bin/qdbuscpp2xml4|g" \
+	-e "s|\$(OE_QMAKE_QDBUSXML2CPP)|$OECORE_NATIVE_SYSROOT/usr/bin/qdbusxml2cpp4|g" \
+	-e "s|\$(OE_QMAKE_QT_CONFIG)|$OECORE_TARGET_SYSROOT/usr/share/qtopia/mkspecs/qconfig.pri|g" \
+	${SDKTARGETSYSROOT}/usr/share/qtopia/mkspecs/common/*.conf
+
+printf "Updating qt5 mkspecs\n"
+sed -i \
+	-e "s|\$\$(OE_QMAKE_CFLAGS)|${CFLAGS}|g" \
+	-e "s|\$\$(OE_QMAKE_CXXFLAGS)|${CXXFLAGS}|g" \
+	-e "s|\$\$(OE_QMAKE_LDFLAGS)|${LDFLAGS}|g" \
+	-e "s|\$\$(OE_QMAKE_CC)|${CC}|g" \
+	-e "s|\$\$(OE_QMAKE_CXX)|${CXX}|g" \
+	-e "s|\$\$(OE_QMAKE_LINK)|${CXX}|g" \
+	-e "s|\$\$(OE_QMAKE_AR)|${AR}|g" \
+	${SDKTARGETSYSROOT}/usr/lib/qt5/mkspecs/linux-oe-g++/qmake.conf
--- meta-venus/recipes-qt/meta/meta-toolchain-qte.bbappend
+++ meta-venus/recipes-qt/meta/meta-toolchain-qte.bbappend
@@ -6,3 +6,21 @@ TOOLCHAIN_TARGET_TASK += "packagegroup-qt5-toolchain-target"
 
 TOOLCHAIN_HOST_TASK += "nativesdk-packagegroup-qt5-toolchain-host qt5-conf"
 TOOLCHAIN_HOST_TASK += "nativesdk-python-importlib nativesdk-python-json"
+
+# The part below hardcodes the paths / commands in the mkspecs of the Qt SDK,
+# so they work from Qt Creator without having to source the envirmental script.
+# The script below runs after extracting the SDK so its installed location
+# is known.
+FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
+
+SRC_URI += "file://hardcode-qt-mkspec-paths.sh"
+
+create_sdk_files_append() {
+	cp ${WORKDIR}/hardcode-qt-mkspec-paths.sh ${SDK_OUTPUT}/${SDKPATH}/
+}
+
+# See openembedded-core/meta/files/toolchain-shar-extract.sh and
+# populate_sdk.bbclass. The @SDK_POST_INSTALL_COMMAND@ in toolchain-shar-extract.sh
+# is replaced by the SDK_POST_INSTALL_COMMAND variable. Since SDK_POST_INSTALL_COMMAND
+# already contains the relocate code, append to it..
+SDK_POST_INSTALL_COMMAND_append = "bash $target_sdk_dir/hardcode-qt-mkspec-paths.sh ${env_setup_script}"
