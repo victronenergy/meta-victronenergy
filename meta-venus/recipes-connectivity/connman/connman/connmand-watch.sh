eae9c56bb7adaee6ded73e5f8ec11f34236ad43c
Mans Rullgard <mans@mansr.com>
2017-12-09 13:07:46 +0000

    connman: pass all arguments from connmand_watch to connmand

--- meta-venus/recipes-connectivity/connman/connman/connmand-watch.sh
+++ meta-venus/recipes-connectivity/connman/connman/connmand-watch.sh
@@ -2,7 +2,7 @@
 
 watch() {
         while [ true ]; do
-                connmand "$1" --nodaemon > /dev/null 2>&1
+                connmand "$@" --nodaemon > /dev/null 2>&1
                 logger -t connmand_watch -p user.error  "the connmand process unexpectedly stopped ($?), restarting"
         done
 }
