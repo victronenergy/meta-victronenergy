7a23303853f3413e2369080c2cf7a71503fcf90d
Jeroen Hofstee <jhofstee@victronenergy.com>
2018-05-03 13:56:52 +0200

    swupdate: add resize2fs.sh, resize2fs of the running rootfs

---
+++ meta-venus/recipes-support/swupdate-scripts/files/resize2fs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+. $(dirname $0)/functions.sh
+
+root=$(get_rootdev $(get_rootfs))
+if [ -z "$root" ]; then
+    echo "Unable to determine rootfs. Exit."
+    exit 1
+fi
+
+fs=$(grep -E '^\S+\s+/\s' /proc/mounts | awk '{print $3}')
+if [ "$fs" != "ext4" ]; then
+    echo "Rootfs is not ext4, but $fs. Exit."
+    exit 1
+fi
+
+echo "resizing $root"
+resize2fs $root
