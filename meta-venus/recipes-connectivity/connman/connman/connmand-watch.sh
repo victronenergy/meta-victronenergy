853fc579c2b9a70065ca8479e911a262582fcc44
Jeroen Hofstee <jhofstee@victronenergy.com>
2018-05-23 13:20:29 +0200

    connmand: don't restart when rebooting

--- meta-venus/recipes-connectivity/connman/connman/connmand-watch.sh
+++ meta-venus/recipes-connectivity/connman/connman/connmand-watch.sh
@@ -3,6 +3,12 @@
 watch() {
         while [ true ]; do
                 connmand "$@" --nodaemon > /dev/null 2>&1
+
+                if [  "$(runlevel | awk '{ print $2 }')" = "6" ]; then
+                        logger -t connmand_watch -p user.info  "no longer restarting the connmand process"
+                        exit
+                fi
+
                 logger -t connmand_watch -p user.error  "the connmand process unexpectedly stopped ($?), restarting"
         done
 }
