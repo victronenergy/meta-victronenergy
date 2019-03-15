victronenergy/meta-victronenergy

commit ce7b76354c16db8e1ecbb3c3536ad548a8e9a818

watchdog: add test-prescaler to run test less often
(and fix the permission of the other patch)

meta-venus/recipes-extended/watchdog/files/0001-add-test-prescaler-to-run-test-less-often.patch
@@ -1,0 +1,71 @@
+From 92e96dc3a898b9bd81b2fb36231d117c54a3f59a Mon Sep 17 00:00:00 2001
+From: Jeroen Hofstee <jhofstee@victronenergy.com>
+Date: Thu, 10 Jan 2019 16:49:30 +0100
+Subject: [PATCH] add test-prescaler to run test less often
+
+When set in the config file, test are only run every n intervals.
+---
+ include/extern.h | 1 +
+ src/configfile.c | 3 +++
+ src/watchdog.c   | 5 +++--
+ 3 files changed, 7 insertions(+), 2 deletions(-)
+
+diff --git a/include/extern.h b/include/extern.h
+index f00e4cf..97c3c41 100644
+--- a/include/extern.h
++++ b/include/extern.h
+@@ -82,6 +82,7 @@ extern char *devname;
+ extern char *admin;
+ 
+ extern int	test_timeout;		/* test-binary time out value. */
++extern int	test_prescaler;
+ extern int	repair_timeout;		/* repair-binary time out value. */
+ extern int	dev_timeout;		/* Watchdog hardware time-out. */
+ extern int	retry_timeout;		/* Retry on non-critical errors. */
+diff --git a/src/configfile.c b/src/configfile.c
+index a0996e2..248b9d9 100644
+--- a/src/configfile.c
++++ b/src/configfile.c
+@@ -51,6 +51,7 @@ static void add_test_binaries(const char *path);
+ #define TEMPPOWEROFF   		"temp-power-off"
+ #define TESTBIN			"test-binary"
+ #define TESTTIMEOUT		"test-timeout"
++#define TESTPRESCALER		"test-prescaler"
+ #define HEARTBEAT		"heartbeat-file"
+ #define HBSTAMPS		"heartbeat-stamps"
+ #define LOGDIR			"log-dir"
+@@ -86,6 +87,7 @@ char *devname = NULL;
+ char *admin = "root";
+ 
+ int test_timeout = TIMER_MARGIN;   /* test-binary time out value. */
++int test_prescaler = 1;
+ int repair_timeout = TIMER_MARGIN; /* repair-binary time out value. */
+ int dev_timeout = TIMER_MARGIN;    /* Watchdog hardware time-out. */
+ int retry_timeout = TIMER_MARGIN;  /* Retry on non-critical errors. */
+@@ -199,6 +201,7 @@ void read_config(char *configfile)
+ 		} else if (READ_INT(REPAIRTIMEOUT, &repair_timeout) == 0) {
+ 		} else if (READ_LIST(TESTBIN, &tr_bin_list) == 0) {
+ 		} else if (READ_INT(TESTTIMEOUT, &test_timeout) == 0) {
++        } else if (READ_INT(TESTPRESCALER, &test_prescaler) == 0) {
+ 		} else if (READ_STRING(HEARTBEAT, &heartbeat) == 0) {
+ 		} else if (READ_INT(HBSTAMPS, &hbstamps) == 0) {
+ 		} else if (READ_STRING(ADMIN, &admin) == 0) {
+diff --git a/src/watchdog.c b/src/watchdog.c
+index 486384a..243a11c 100644
+--- a/src/watchdog.c
++++ b/src/watchdog.c
+@@ -549,8 +549,9 @@ int main(int argc, char *const argv[])
+ 				  act->parameter.net.packet, tint, pingcount), repair_bin, act);
+ 
+ 		/* test, or test/repair binaries in the watchdog.d directory */
+-		for (act = tr_bin_list; act != NULL; act = act->next)
+-			do_check(check_bin(act->name, test_timeout, act->version), repair_bin, act);
++		if ((count % test_prescaler) == 0)
++			for (act = tr_bin_list; act != NULL; act = act->next)
++				do_check(check_bin(act->name, test_timeout, act->version), repair_bin, act);
+ 
+ 		/* in case test binaries return quickly */
+ 		usleep(swait);
+-- 
+2.7.4
+

meta-venus/recipes-extended/watchdog/watchdog_%.bbappend
@@ -10,6 +10,7 @@ SRC_URI += "\
 	file://store_watchdog_error.sh \
 	file://vrm-online.sh \
 	file://0001-Use-MemAvailable-instead-of-MemFree.patch \
+	file://0001-add-test-prescaler-to-run-test-less-often.patch \
 "
 
 INITSCRIPT_PACKAGES = "${PN}"

commit e48f95f453819216ebed6f5265251cf181a4cb3d

watchdog: react earlier on network outages
When testing the network watchdog it was noted that reboots can be severly
delayed. Partly caused by ce7b7635 "watchdog: add test-prescaler to run test
less often" only calls check_bin ones every 2 minutes. However this is not a
blocking call but a polling one, so the result is also known 2 minutes later.
Fix that by calling the poll routine every second, but only start a new
process every now and then.

The watchdog also the checks for failure again for retry-timeout time. Since
the script already takes care of potential delays, there is no need for this
additional delay. Just reboot once the script says it is not ok.

Lower the check from 2 minutes to 1 minute, since 1 minute is the minumum
time used by vrm and the timeout as well. So if both are set to 1 minute,
the device will be rebooted within 2-3 minutes after connection lose.

Since the device now reacts faster to no vrm connectivity, it can end up in
an endless reboot loop since it takes quite some time before network and
vrmlogger is up and running. So keep a minimum delay of 5 minutes when there
was no connectivity with vrm yet.

meta-venus/recipes-extended/watchdog/files/0001-add-test-prescaler-to-run-test-less-often.patch
@@ -1,17 +1,18 @@
-From 92e96dc3a898b9bd81b2fb36231d117c54a3f59a Mon Sep 17 00:00:00 2001
+From d2fd1df5e8e0e3875916b0467eff8f68314c8e53 Mon Sep 17 00:00:00 2001
 From: Jeroen Hofstee <jhofstee@victronenergy.com>
-Date: Thu, 10 Jan 2019 16:49:30 +0100
+Date: Thu, 14 Mar 2019 13:24:29 +0000
 Subject: [PATCH] add test-prescaler to run test less often
 
 When set in the config file, test are only run every n intervals.
 ---
- include/extern.h | 1 +
- src/configfile.c | 3 +++
- src/watchdog.c   | 5 +++--
- 3 files changed, 7 insertions(+), 2 deletions(-)
+ include/extern.h  | 3 ++-
+ src/configfile.c  | 3 +++
+ src/test_binary.c | 5 ++++-
+ src/watchdog.c    | 4 ++--
+ 4 files changed, 11 insertions(+), 4 deletions(-)
 
 diff --git a/include/extern.h b/include/extern.h
-index f00e4cf..97c3c41 100644
+index f00e4cf..e09119e 100644
 --- a/include/extern.h
 +++ b/include/extern.h
 @@ -82,6 +82,7 @@ extern char *devname;
@@ -22,6 +23,15 @@ index f00e4cf..97c3c41 100644
  extern int	repair_timeout;		/* repair-binary time out value. */
  extern int	dev_timeout;		/* Watchdog hardware time-out. */
  extern int	retry_timeout;		/* Retry on non-critical errors. */
+@@ -152,7 +153,7 @@ int check_temp(struct list *act);
+ int close_tempcheck(void);
+ 
+ /** test_binary.c **/
+-int check_bin(char *, int, int);
++int check_bin(char *, int, int, int);
+ void free_process(void);
+ 
+ /** pidfile.c **/
 diff --git a/src/configfile.c b/src/configfile.c
 index a0996e2..248b9d9 100644
 --- a/src/configfile.c
@@ -50,22 +60,47 @@ index a0996e2..248b9d9 100644
  		} else if (READ_STRING(HEARTBEAT, &heartbeat) == 0) {
  		} else if (READ_INT(HBSTAMPS, &hbstamps) == 0) {
  		} else if (READ_STRING(ADMIN, &admin) == 0) {
+diff --git a/src/test_binary.c b/src/test_binary.c
+index 2211f9e..d3e07d6 100644
+--- a/src/test_binary.c
++++ b/src/test_binary.c
+@@ -235,7 +235,7 @@ static int check_processes(const char *name, int *ecode)
+  * A time-out of zero will disable the time-out checking, but in that case a blocked child
+  * will simply persist indefinitely and no error will be found.
+  */
+-int check_bin(char *tbinary, int timeout, int version)
++int check_bin(char *tbinary, int timeout, int version, int dorun)
+ {
+ 	pid_t child_pid;
+ 	int ecode = EDONTKNOW;
+@@ -256,6 +256,9 @@ int check_bin(char *tbinary, int timeout, int version)
+ 		return EDONTKNOW;
+ 	}
+ 
++	if (!dorun)
++		return ecode;
++
+ 	child_pid = fork();
+ 	if (!child_pid) {
+ 
 diff --git a/src/watchdog.c b/src/watchdog.c
-index 486384a..243a11c 100644
+index 486384a..869dee9 100644
 --- a/src/watchdog.c
 +++ b/src/watchdog.c
-@@ -549,8 +549,9 @@ int main(int argc, char *const argv[])
- 				  act->parameter.net.packet, tint, pingcount), repair_bin, act);
+@@ -550,11 +550,11 @@ int main(int argc, char *const argv[])
  
  		/* test, or test/repair binaries in the watchdog.d directory */
--		for (act = tr_bin_list; act != NULL; act = act->next)
+ 		for (act = tr_bin_list; act != NULL; act = act->next)
 -			do_check(check_bin(act->name, test_timeout, act->version), repair_bin, act);
-+		if ((count % test_prescaler) == 0)
-+			for (act = tr_bin_list; act != NULL; act = act->next)
-+				do_check(check_bin(act->name, test_timeout, act->version), repair_bin, act);
++			do_check(check_bin(act->name, test_timeout, act->version, (count % test_prescaler) == 0), repair_bin, act);
  
  		/* in case test binaries return quickly */
  		usleep(swait);
+-		check_bin(NULL, test_timeout, 0);
++		check_bin(NULL, test_timeout, 0, 0);
+ 
+ 		/* finally sleep for a full cycle */
+ 		/* we have just triggered the device with the last check */
 -- 
 2.7.4
 

meta-venus/recipes-extended/watchdog/files/vrm-online.sh
@@ -25,13 +25,18 @@ do_test() {
     interval=$(get_value settings /Settings/Vrmlogger/LogInterval)
     last=$(get_value logger /Vrm/TimeLastContact)
     now=$(date +%s)
+    totaltimeout=$((interval + timeout))
 
     if [ -z "$last" ]; then
         # no contact yet, use boot time
         last=$(awk '/^btime/ { print $2 }' /proc/stat)
+        # make sure there is at least a 5 minutes time on a clean boot
+        if [ $totaltimeout -lt 300 ]; then
+            totaltimeout=300
+        fi
     fi
 
-    if [ $now -gt $((last + interval + timeout)) ]; then
+    if [ $now -gt $((last + totaltimeout)) ]; then
         logger "VRM unreachable, rebooting"
         exit -10
     fi

meta-venus/recipes-extended/watchdog/files/watchdog.conf
@@ -5,4 +5,5 @@ min-memory = 2500
 max-load-5 = 6
 max-load-15 = 6
 repair-binary = /usr/sbin/store_watchdog_error.sh
-test-prescaler = 120
+test-prescaler = 60
+retry-timeout = 0