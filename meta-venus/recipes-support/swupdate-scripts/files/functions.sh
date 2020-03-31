5b678ec2134ae72f2ac3543b8833af77b8651307
Jeroen Hofstee <jhofstee@victronenergy.com>
2020-03-31 14:46:14 +0200

    swupdate-scripts: be compatible with gsed
    gsed comlains about get_version with: "sed: -e expression #1,
    char 3: extra characters after command". Seperate the commands
    with a semicolon to make it work.

--- meta-venus/recipes-support/swupdate-scripts/files/functions.sh
+++ meta-venus/recipes-support/swupdate-scripts/files/functions.sh
@@ -3,7 +3,7 @@
 # print the version and timestamp as one line
 # optional argument overrides version file
 get_version() {
-    sed -n -e '1h3{Gs/\n/ /p}' "${1:-/opt/victronenergy/version}"
+    sed -n -e '1h;3{G;s/\n/ /p}' "${1:-/opt/victronenergy/version}"
 }
 
 # print the number of the backup rootfs
