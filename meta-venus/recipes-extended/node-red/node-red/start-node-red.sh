283445be9a0cb88c018dec9f10821893025a4fee
Dirk-Jan Faber <dfaber@victronenergy.com>
2021-08-13 13:00:45 +0200

    Now installing user-authentication.js to the rootfs instead of /data.
    This allows us to utilize the bcryptjs and debug npm modules from
    nodered, which makes installing those no longer needed.

--- meta-venus/recipes-extended/node-red/node-red/start-node-red.sh
+++ meta-venus/recipes-extended/node-red/node-red/start-node-red.sh
@@ -10,7 +10,6 @@ DEFAULTCONF="/usr/lib/node_modules/node-red/defaults"
 if [ ! -d $NODE_RED ]; then
 	mkdir $NODE_RED
 	mkdir $DATA_MODULES
-	cp "$DEFAULTCONF/user-authentication.js" "$NODE_RED"
 	cp "$DEFAULTCONF/settings.js" "$NODE_RED"
 	sed -i "s/a-secret-key/`openssl rand -hex 32`/g" "$NODE_RED/settings.js"
 fi
@@ -22,11 +21,5 @@ fi
 if [ ! -f $VICTRON ]; then
 	touch $VICTRON
 fi
-
-for MODULE in bcryptjs debug; do
-	if [ ! -d $DATA_MODULES/${MODULE} ]; then
-		(cd $NODE_RED; npm install ${MODULE})
-	fi
-done
 
 exec /usr/lib/node_modules/node-red/red.js $@ --userDir ${NODE_RED}
