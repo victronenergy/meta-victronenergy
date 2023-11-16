01183b19649b3329f4461919ade687531f6ccbe0
Jeroen Hofstee <jhofstee@victronenergy.com>
2023-11-16 16:37:20 +0100

    remount-rw: add a (dummy) cat statement to check for errors
    The busybox awk happily returns success even if the file sytem is full etc. Hence
    pipe it through cat so such errors can be catched.

--- meta-venus/recipes-support/swupdate-scripts/files/remount-rw.sh
+++ meta-venus/recipes-support/swupdate-scripts/files/remount-rw.sh
@@ -4,5 +4,9 @@
 if ! awk '{ if ( $2 ~ "^/$" && $4 ~ /(^|,)ro($|,)/ ) { exit 1 } }' /proc/mounts; then
 	echo "making the rootfs writable..."
 	mount -o remount,rw /
-	awk '$2~"^/$"{$4="defaults"}1' /etc/fstab > /etc/fstab.tmp && sync && mv /etc/fstab.tmp /etc/fstab && sync
+	# MIND IT, the weird cat below is because the busybox awk doesn't check if it
+	# can write to stdout, it will simply fail successfully. This can result in ending
+	# up with an empty fstab and hence a bricked device. So keep the cat or fix busybox
+	# or require gawk...
+	awk '$2~"^/$"{$4="defaults"}1' /etc/fstab | cat > /etc/fstab.tmp && sync && mv /etc/fstab.tmp /etc/fstab && sync
 fi
