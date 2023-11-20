ce1059f99894033cdd271ebb20b0e816bdce7da9
Jeroen Hofstee <jhofstee@victronenergy.com>
2023-11-20 11:10:09 +0100

    resize2fs: modify /etc/fstab after resizing
    We managed to create an image with so little free space, that modifying /etc/fstab was
    not possible. Since busybox its awk doesn't error about that, we ended up with an empty
    fstab. This moves the modification of fstab till after the resizing and adds a
    workaround for the lack of error reporting.

--- meta-venus/recipes-support/swupdate-scripts/files/resize2fs.sh
+++ meta-venus/recipes-support/swupdate-scripts/files/resize2fs.sh
@@ -8,13 +8,28 @@ if [ -z "$root" ]; then
     exit 1
 fi
 
-$(dirname "$0")/remount-rw.sh
+# make the rootfs writable and keep it writable after reboot
 
-fs=$(grep -E '^\S+\s+/\s' /proc/mounts | awk '{print $3}')
-if [ "$fs" != "ext4" ]; then
-    echo "Rootfs is not ext4, but $fs. Exit."
-    exit 0
+if ! awk '{ if ( $2 ~ "^/$" && $4 ~ /(^|,)ro($|,)/ ) { exit 1 } }' /proc/mounts; then
+	was_ro="1"
+    echo "making the rootfs writable..."
+    mount -o remount,rw /
 fi
 
-echo "resizing $root"
-resize2fs $root
+fs=$(grep -E '^\S+\s+/\s' /proc/mounts | awk '{print $3}')
+if [ "$fs" == "ext4" ]; then
+    echo "resizing $root"
+    resize2fs $root
+else
+    echo "Rootfs is not ext4, but $fs"
+fi
+
+if [ "$was_ro" = "1" ]; then
+	# MIND IT, the weird cat below is because the busybox awk doesn't check if it
+	# can write to stdout, it will simply fail successfully. This can result in ending
+	# up with an empty fstab and hence a bricked device. So keep the cat or fix busybox
+	# or require gawk...
+	echo "modifying /etc/fstab"
+	awk '$2~"^/$"{$4="defaults"}1' /etc/fstab | cat > /etc/fstab.tmp && sync && mv /etc/fstab.tmp /etc/fstab && sync
+fi
+
