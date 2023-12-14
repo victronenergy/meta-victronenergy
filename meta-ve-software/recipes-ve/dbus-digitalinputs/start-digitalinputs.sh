a01d9c8ceb1fd8539257c59c5700094413fab6f5
Mans Rullgard <mans@mansr.com>
2023-12-14 10:43:17 +0000

    dbus-digitalinputs: support USB I/O extender

--- meta-ve-software/recipes-ve/dbus-digitalinputs/start-digitalinputs.sh
+++ meta-ve-software/recipes-ve/dbus-digitalinputs/start-digitalinputs.sh
@@ -1,12 +1,20 @@
 #! /bin/sh
 
-if ! test -e /dev/gpio/digital_input_1; then
-    svc -d .
+set --
+
+if [ -e /dev/gpio/digital_input_1 ]; then
+    set -- /dev/gpio/digital_input_*
+fi
+
+if [ -d /run/io-ext ]; then
+    for f in $(find /run/io-ext -name pins.conf); do
+        set -- "$@" --conf $f
+    done
+fi
+
+if [ -z "$*" ]; then
+    svd -d .
     exit 0
 fi
 
-if ! test -e /dev/gpio/digital_input_1/edge; then
-    flags=--poll=poll
-fi
-
-exec $(dirname $0)/dbus_digitalinputs.py $flags /dev/gpio/digital_input_*
+exec $(dirname $0)/dbus_digitalinputs.py "$@"
