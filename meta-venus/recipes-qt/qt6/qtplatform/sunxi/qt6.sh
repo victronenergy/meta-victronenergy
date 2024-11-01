96378cb8d4ca21bb140cfb2540ec34bfaee2bdd4
Jeroen Hofstee <jhofstee@victronenergy.com>
2024-11-01 16:25:52 +0100

    qt6: sunxi: show a cursor if a mouse is connected

--- meta-venus/recipes-qt/qt6/qtplatform/sunxi/qt6.sh
+++ meta-venus/recipes-qt/qt6/qtplatform/sunxi/qt6.sh
@@ -1,4 +1,5 @@
 export QT_QPA_PLATFORM=eglfs
 export QT_QPA_EGLFS_INTEGRATION=eglfs_kms
+export QT_QPA_EGLFS_HIDECURSOR=0
 export QT_QPA_KMS_CONFIG=/etc/qt-kms.conf
 export XDG_RUNTIME_DIR=/var/volatile/tmp/runtime-root
