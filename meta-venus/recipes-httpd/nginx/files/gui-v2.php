2f5fa31b36efcbb8cc12a73579a1ad7fa06fb3e2
Manuel <mr-manuel@outlook.it>
2025-06-20 12:17:19 +0200

    Forward query parameter for GUIv2
    This is needed to be able to open the new GUIv2 without mockup
    and in full screen mode on a MFD device.

--- meta-venus/recipes-httpd/nginx/files/gui-v2.php
+++ meta-venus/recipes-httpd/nginx/files/gui-v2.php
@@ -1,6 +1,13 @@
 <?php
+	$queryString = $_SERVER['QUERY_STRING'];
+	$newLocation = '/gui-v2';
+
+	// Check if there are any query parameters
+	if (!empty($queryString)) {
+		$newLocation .= '/?' . $queryString;
+	}
+
 	http_response_code(302);
-	header("Location: /gui-v2");
+	header("Location: $newLocation");
 	exit();
 ?>
-
