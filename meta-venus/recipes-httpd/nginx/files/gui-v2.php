7a64427c008caba4f885db9f0cbb95930ce4dedb
Jeroen Hofstee <jhofstee@victronenergy.com>
2025-06-25 12:55:27 +0200

    nginx: forward query parameter for GUIv2
    This is needed to be able to open the new GUIv2 without mockup
    and in full screen mode on a MFD device.

--- meta-venus/recipes-httpd/nginx/files/gui-v2.php
+++ meta-venus/recipes-httpd/nginx/files/gui-v2.php
@@ -1,6 +1,6 @@
 <?php
 	http_response_code(302);
-	header("Location: /gui-v2");
+	header("Location: /gui-v2" . (empty($_SERVER['QUERY_STRING']) ? "" : "/?" . $_SERVER['QUERY_STRING']));
 	exit();
 ?>
 
