9363c209cb3bfa7fd02221f4478db489f443b2fa
Jeroen Hofstee <jhofstee@victronenergy.com>
2025-11-20 15:12:58 +0100

    signalk: find a default nmea2000 CAN interface
    Instead of hard coding a default NMEA 2000 interface, just query
    the network interfaces. This also makes signalk not machine specific.
    
    This will also update any existing settings file, but only if the
    configured CAN-bus interface doesn't exists at all.
    
    https://github.com/victronenergy/venus/issues/1314

--- meta-third-party/recipes-extended/signalk-server/signalk-server/start-signalk.sh
+++ meta-third-party/recipes-extended/signalk-server/signalk-server/start-signalk.sh
@@ -6,6 +6,49 @@ CONF="/data/conf/signalk"
 PLUGINCONF="${CONF}/plugin-config-data"
 DEFAULTCONF="/usr/lib/node_modules/signalk-server/defaults"
 
+n2k_interface=""
+
+# - prefer vecan* over can* interfaces (no config or USB ones)
+# - skip the bms-only ones.
+# - prefer VE.Can as name (the Venus GX like devices have a non isolated can0,
+#   called CAN 2 Port, while VE.Can is can1)
+# - return the first matching one, e.g. vecan0 over vecan1 and can0 over can1.
+#   (it is a shell script, it can't return values, it will be in n2k_interface)
+find_n2k_canbus() {
+    n2k_interface=""
+    for f in /sys/class/net/*; do
+        if [ ! -e "$f" ]; then
+             break
+        fi
+
+        type=$(cat $f/type)
+        if [ "$type" != "280" ]; then
+            continue
+        fi
+
+        interface=$(basename $f)
+        config=$(udevadm info -q property $f | sed -n  's/^VE_CAN_CONFIG=\(.*\)/\1/p')
+        name=$(udevadm info -q property $f | sed -n  's/^VE_NAME=\(.*\)/\1/p')
+
+        if [ "$config" != "bms-only" ]; then
+
+            if [ "$n2k_interface" = "" ]; then
+                n2k_interface="$interface"
+            fi
+
+            if [[ "$interface" =~ ^vecan.* ]]; then
+                if ! [[ "$n2k_interface" =~ ^vecan.* ]]; then
+                    n2k_interface="$interface"
+                fi
+            fi
+            if [[ $name =~ ^VE.Can.* ]]; then
+                n2k_interface="$interface"
+                break
+            fi
+        fi
+    done
+}
+
 # Remove plugins from /data that are, since v3.00, installed on root
 # npm uninstall is used, rather than for example rm -rf, to also remove
 # them from $CONF/package.json.
@@ -23,7 +66,31 @@ fi
 mkdir -p /data/conf/signalk/plugin-config-data
 
 if [ ! -f "$CONF/settings.json" ]; then
-    cp "$DEFAULTCONF/settings.json" "$CONF"
+    find_n2k_canbus
+    if [ "$n2k_interface" != "" ]; then
+        canbus_conf=$(sed "s/%CAN_INTERFACE%/$n2k_interface/g" "$DEFAULTCONF/canbus.json")
+        jq '.pipedProviders += ['"$canbus_conf"']' < "$DEFAULTCONF/settings.json" > "$CONF/settings.json"
+    else
+        cp "$DEFAULTCONF/settings.json" "$CONF"
+    fi
+else
+    settings_n2k=$(jq -r '.pipedProviders[] |
+                    select(.id == "n2k-on-ve.can-socket") |
+                    .pipeElements[].options.subOptions.interface' \
+                    /data/conf/signalk/settings.json)
+
+    if [ ! -e "/sys/class/net/$settings_n2k" ]; then
+        echo "The configured $settings_n2k CAN-bus doesn't exists!"
+        find_n2k_canbus
+        if [ "$n2k_interface" != "" ]; then
+            echo "Changing it to $n2k_interface"
+            jq --arg iface "$n2k_interface" '(.pipedProviders[] |
+                    select(.id == "n2k-on-ve.can-socket") |
+                    .pipeElements[].options.subOptions.interface) = $iface' \
+                /data/conf/signalk/settings.json > /data/conf/signalk/settings.json.tmp
+            mv /data/conf/signalk/settings.json.tmp /data/conf/signalk/settings.json
+        fi
+    fi
 fi
 
 if [ ! -f "$CONF/defaults.json" ]; then
