797cc844f55c75da106cbeccd265695c694ef3d0
Mans Rullgard <mans@mansr.com>
2026-01-30 21:26:09 +0000

    swupdate-scripts: add machine file for k3

---
+++ meta-venus/recipes-support/swupdate-scripts/files/k3/machine.sh
@@ -0,0 +1,24 @@
+rootfstype=ext4
+
+# print the number of the current rootfs
+get_rootfs() {
+    dev=$(stat -c %d /)
+    case $((dev & 31)) in
+        2) echo 1 ;;
+        3) echo 2 ;;
+    esac
+}
+
+# print device name of specified rootfs number
+get_rootdev() {
+    echo /dev/mmcblk0p$(($1 + 1))
+}
+
+do_swupdate() {
+    unlock_env
+    nice -n5 swupdate "$@"
+}
+
+unlock_env() {
+    echo 0 >/sys/block/mmcblk0boot1/force_ro
+}
