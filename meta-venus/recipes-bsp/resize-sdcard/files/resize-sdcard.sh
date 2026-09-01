0d4475371720d92fee6e3476b950299269390606
nmbath <mark@baggywrinkle.co.uk>
2026-09-01 08:52:35 +0100

    resize-sdcard: use fixed-size root partitions

--- meta-venus/recipes-bsp/resize-sdcard/files/resize-sdcard.sh
+++ meta-venus/recipes-bsp/resize-sdcard/files/resize-sdcard.sh
@@ -1,23 +1,37 @@
 #!/bin/sh
 
+DEVICE=/dev/mmcblk0
+ROOT_SIZE_MIB=4096
+
 mount -o remount,rw /
 
-LAST_PART_NUM=$(parted /dev/mmcblk0 -ms unit s p | tail -n 1 | cut -f 1 -d:)
+partition_table=$(parted "$DEVICE" -ms unit s print)
+LAST_PART_NUM=$(echo "$partition_table" | tail -n 1 | cut -f 1 -d:)
 
 if [ $LAST_PART_NUM -ne 2 ]; then
 	echo "more then two partions already exist"
 	exit 0
 fi
 
-size=$(cat /sys/class/block/mmcblk0/size)
+sector_size=$(cat /sys/class/block/mmcblk0/queue/logical_block_size)
+device_sectors=$(echo "$partition_table" | awk -F: '/^\/dev\// { sub(/s$/, "", $2); print $2 }')
+root_a_start=$(echo "$partition_table" | awk -F: '$1 == "2" { sub(/s$/, "", $2); print $2 }')
+root_size_sectors=$((ROOT_SIZE_MIB * 1024 * 1024 / sector_size))
+root_a_end=$((root_a_start + root_size_sectors - 1))
+root_b_start=$((root_a_end + 1))
+root_b_end=$((root_b_start + root_size_sectors - 1))
+data_start=$((root_b_end + 1))
 
-if [ $size -lt 1562500 ]; then
+if [ -z "$root_a_start" ] || [ "$data_start" -ge "$device_sectors" ]; then
 	echo "sd card is too small"
 	exit 1
 fi
 
 echo "MIND IT: CHANGING THE MBR!!!"
-parted /dev/mmcblk0 -ms resizepart 2 33% mkpart primary ext4 33% 66% mkpart primary ext4 66% 100%
+parted "$DEVICE" -ms unit s \
+	resizepart 2 "${root_a_end}s" \
+	mkpart primary ext4 "${root_b_start}s" "${root_b_end}s" \
+	mkpart primary ext4 "${data_start}s" 100%
 echo "DONE!!!"
 
 mkfs.ext4 -F /dev/mmcblk0p3
